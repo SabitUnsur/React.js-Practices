d0229d64db74579c8288abc3e758e4cdfde8f61b
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-12 14:33:08 +0300

    revised

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   // paragraph.textContent = "Hello World"
   // document.getElementById("root").append(paragraph)
   // bu şekilde yazmak uzun ve zahmetli ve yavaş bir işlem olur. Jsx ile bu işlemi daha kolay yapabiliriz. Jsx bize html ve js kodlarını bir arada kullanmamızı sağlar.
+  //props: componentler arasında veri taşımak için kullanılır.
 
   return (
     <div className="App">
